c912edc4f47686e62f6cbe46fc3dee66337900ae
usersudarshan <user.sudarshan@gmail.com>
2025-07-19 21:31:56 +0530

    Restore token on load

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import { setAuthToken } from "./services/api";
+
+//Restore token on load
+const token = localStorage.getItem("token");
+if (token) {
+  setAuthToken(token);
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
